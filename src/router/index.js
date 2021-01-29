3513bdd09b76ddde9c48978e195d28fda32dacca
LaiWeifeng <laiweifeng@hotmail.com>
2021-01-29 15:49:11 +0800

    c

--- src/router/index.js
+++ src/router/index.js
@@ -5,25 +5,42 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    // {
+    //     path: '/',
+    //     name: 'Login',
+    //     component: () => import('../views/Login.vue')
+    // },
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('../views/Home.vue'),
+        children: [
+            {
+                path: '/dashboard',
+                name: 'Dashboard',
+                component: () => import('../views/Dashboard.vue')
+            },
+        ]
+
+    },
+    {
+        path: '/403',
+        component: () => import('../views/403.vue'),
+    },
+    {
+        path: '/404',
+        component: () => import('../views/404.vue'),
+    },
+    {
+        path: '*',
+        redirect: '/404'
+    },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+    <h4>我是子路由</h4>
+</template>
+
+<script>
+    export default {
+        name: "Dashboard"
+    }
+</script>
+
+<style scoped>
+
+</style>
